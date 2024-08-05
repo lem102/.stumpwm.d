15229b7237ab3a1baac65fc14e556d8712b41a6e
Jacob Leeming <jacob.leeming@tappit.com>
2024-08-05 21:11:15 +0100

    loads of stuff!

--- init.lisp
+++ init.lisp
@@ -1,8 +1,9 @@
 (in-package :stumpwm)
 
-;; to start server for sly
-;; (ql:quickload :slynk)
-;; (slynk:create-server :dont-close t)
+;; start server for sly
+;; (defun jacob-open-connection ()
+;;   (ql:quickload :slynk)
+;;   (slynk:create-server :dont-close t))
 
 ;; set prefix key
 (set-prefix-key (kbd "s-t"))
@@ -11,38 +12,64 @@
 (toggle-mode-line (current-screen)
                   (current-head))
 
+;; parameters
+
+(defparameter *jacob-browser* "firefox"
+  "Browser program to use.")
+
+;; (setq *jacob-browser* "google-chrome")
+
 ;; commands
-(defcommand jacob-firefox () ()
-  "Run or raise firefox."
-  (run-or-raise "firefox" '(:class "Firefox")))
+
+;; JACOBTODO: support multiple browsers with one command
+(defcommand jacob-browser () ()
+  "Run or raise the browser."
+  (run-or-raise *jacob-browser* '(:role "browser")))
 
 (defcommand jacob-terminal () ()
   "Run or raise the terminal."
   (run-or-raise "xfce4-terminal" '(:class "Xfce4-terminal")))
 
+(defcommand jacob-vertical-split () ()
+  "Vertically split the screen showing the browser and emacs."
+  (if (only-one-frame-p)
+      (progn
+        (emacs)
+        (hsplit)
+        (move-focus :right)
+        (jacob-browser))
+      (only)))
+
 ;; keybinds
 (define-key *top-map* (kbd "s-1") "emacs")
-(define-key *top-map* (kbd "s-2") "jacob-firefox")
+(define-key *top-map* (kbd "s-2") "jacob-browser")
+(define-key *top-map* (kbd "s-3") "jacob-vertical-split")
 (define-key *top-map* (kbd "s-4") "jacob-terminal")
 (define-key *top-map* (kbd "s-k") "delete")
 
 ;; add date/time to mode line
 (setq *time-modeline-string* "%H:%M %d/%m/%y")
-(setq *screen-mode-line-format* (list "[^B%n^b] %W | %d"))
+(setq *screen-mode-line-format* (list "[^B%n^b] %W | %d %B"))
 (setq *mode-line-timeout* 10)
 
 ;; make mouse change focused window
 (setq *mouse-focus-policy* :sloppy)
 
+
+;; modules
+;; git clone https://github.com/stumpwm/stumpwm-contrib ~/.stumpwm.d/modules
+
+(load-module "battery-portable")
+
 ;; system tray
 
-;; git clone https://github.com/stumpwm/stumpwm-contrib ~/.stumpwm.d/modules
-;; sudo apt install pasystray
+(ql:quickload "xembed")
 
 (load-module "stumptray")
 (stumptray::stumptray)
 
+;; JACOBTODO: is there a more stumpwm way to do this?
+;; sudo apt install pasystray
 (run-shell-command "pasystray")
 
-;; JACOBTODO: create vertical split between emacs and browser
 
